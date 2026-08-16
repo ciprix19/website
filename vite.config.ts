622ca30d2f69573d53a18f7c8a17bc2ec56a39ec
Cipri B <ciprianbarbuta@gmail.com>
2026-08-16 18:24:31 +0300

    dockerfiles added

--- vite.config.ts
+++ vite.config.ts
@@ -4,5 +4,7 @@ import react from '@vitejs/plugin-react';
 // https://vite.dev/config/
 export default defineConfig({
     plugins: [react()],
-    base: '/website/',
+    // used for github pages deploy
+    // base: '/website/',
+    base: '/',
 });
